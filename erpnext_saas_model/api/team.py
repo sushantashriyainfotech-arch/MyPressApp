4363c24033de9e783199c6a7ee22f3b202037614
sushant <sushant.ashriyainfotech@gmail.com>
2026-05-27 12:01:25 +0530

    feat: manually add new api to get current team data

--- erpnext_saas_model/api/team.py
+++ erpnext_saas_model/api/team.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 import frappe
-from press.utils import get_current_team
+from press.utils import get_current_team as _get_current_team
 
 
 @frappe.whitelist()
-def get_current_team(team):
-    team = get_current_team(True)
+def get_current_team():
+    team = _get_current_team(get_doc=True)
     return team
