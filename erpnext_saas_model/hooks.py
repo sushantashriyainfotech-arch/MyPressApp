20e3490471f9a011d09187a04d0692fe8563ff01
sushant <sushant.ashriyainfotech@gmail.com>
2026-06-05 13:57:01 +0530

    feat: let frappe call press api for sync site

--- erpnext_saas_model/hooks.py
+++ erpnext_saas_model/hooks.py
@@ -208,7 +208,8 @@ override_whitelisted_methods = {
 	"press.api.site.change_plan": "erpnext_saas_model.api.site.change_plan",
 	"press.api.team.get_current_team": "erpnext_saas_model.api.team.get_current_team",
     "press.api.site.get_current_subscription_context": "erpnext_saas_model.api.site.get_current_subscription_context",
-	"press.api.site.check_user_creation_eligibility": "erpnext_saas_model.api.site.check_user_creation_eligibility"
+	"press.api.site.check_user_creation_eligibility": "erpnext_saas_model.api.site.check_user_creation_eligibility",
+	"press.api.site.sync_site_user": "erpnext_saas_model.api.site.sync_site_user"
 }
 
 #
